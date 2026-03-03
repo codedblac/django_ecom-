13810de83aefebe07a7d260773e661d77964d048
CODED BLACK <codedb693@gmail.com>
2026-03-03 11:28:23 +0300

    Update forms.py

--- Django-Ecommerce-main/store/forms.py
+++ Django-Ecommerce-main/store/forms.py
@@ -58,7 +58,8 @@ class UpdateUserForm(UserChangeForm):
 		self.fields['username'].label = ''
 		self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
-		
+
+
 class SignUpForm(UserCreationForm):
 	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
 	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
