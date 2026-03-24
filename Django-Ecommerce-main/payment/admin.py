96fc554dedb4cf9b068a26c8df2604ee5bf789d7
CODED BLACK <codedb693@gmail.com>
2026-03-24 23:54:52 +0300

    Update admin.py

--- Django-Ecommerce-main/payment/admin.py
+++ Django-Ecommerce-main/payment/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 
-
 # Register the model on the admin section thing
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
